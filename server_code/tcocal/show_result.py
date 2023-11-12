2466de65a177d8b00b714ea6301e7b706f55542b
07linwei@gmail.com <07linwei@gmail.com>
2023-11-12 03:43:04 +0000

    Edited tcocal.show_result

--- server_code/tcocal/show_result.py
+++ server_code/tcocal/show_result.py
@@ -37,13 +37,6 @@ def show_comparison_figure(aws_region, onefs_contract_term, onefs_license_discou
     annual_aws_ec2_cost[0] = 12 * 730 * costcal.cal_ec2_instance_cost_hourly(aws_region, node_amount, instance_type, ec2_payment_option)
     annual_aws_storage_cost [0] = 12 * 730 * costcal.cal_ebs_cost_hourly(aws_region, disk_type, cluster_raw_capacity_gib)
     annual_license_cost[0] = 12 * costcal.cal_onefs_license_cost_monthly(onefs_contract_term, cluster_raw_capacity_gib, onefs_license_discount)
-    data['OneFS on AWS'][0] = round(annual_aws_ec2_cost[0], 2)
-    data['OneFS on AWS'][1] = round(annual_aws_storage_cost [0], 2)
-    data['OneFS on AWS'][2] = round(annual_license_cost[0], 2)
-    data['OneFS on AWS'][3] = round(annual_aws_ec2_cost[0] + annual_aws_storage_cost [0] + annual_license_cost[0], 2)
-    data['OneFS on AWS'][4] = round(cluster_raw_capacity_gib, 2)
-    data['OneFS on AWS'][5] = round(effective_capacity_tib, 2)
-    data['OneFS on AWS'][6] = round((annual_aws_ec2_cost[0] + annual_aws_storage_cost [0] + annual_license_cost[0])/effective_capacity_tib, 2)
 
     
     print("cluster_raw_capacity_gib:")
@@ -51,19 +44,12 @@ def show_comparison_figure(aws_region, onefs_contract_term, onefs_license_discou
     # Standard Storage EFS related cost
     annual_aws_storage_cost[1] = 12 * costcal.cal_efs_solution_cost_monthly(aws_region, "standard_storage", node_amount, node_disk_amount, node_disk_size, onefs_drr_ratio)
     annual_license_cost[1] = 0
-    data['EFS Standard Storage'][1] = round(annual_aws_storage_cost[1], 2)
-    data['EFS Standard Storage'][3] = round(annual_aws_storage_cost[1], 2)
-    data['EFS Standard Storage'][5] = round(effective_capacity_tib, 2)
-    data['EFS Standard Storage'][6] = round(annual_aws_storage_cost[1]/effective_capacity_tib, 2)
+   
 
     # One Zone Storage EFS related cost
     annual_aws_storage_cost[2] = 12 * costcal.cal_efs_solution_cost_monthly(aws_region, "one_zone_storage", node_amount, node_disk_amount, node_disk_size, onefs_drr_ratio)
     annual_license_cost[2] = 0
-    data['EFS One Zone Storage'][1] = round(annual_aws_storage_cost[2], 2)
-    data['EFS One Zone Storage'][3] = round(annual_aws_storage_cost[2], 2)
-    data['EFS One Zone Storage'][5] = round(effective_capacity_tib, 2)
-    data['EFS One Zone Storage'][6] = round(annual_aws_storage_cost[2]/effective_capacity_tib, 2)
-
+    
     print(annual_aws_storage_cost)
     print(annual_license_cost)
     
